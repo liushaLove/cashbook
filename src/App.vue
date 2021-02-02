3a9277332119b90fb9e13a84d4fa3ab722e73b52
liushaLove <1270842602@qq.com>
2021-02-02 15:42:01 +0800

    提交Nav组件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
 <template>
   <router-view/>
-  <hr/>
-  <div>
-    <router-link to="/money">记账</router-link>
-    |
-    <router-link to="/labels">标签</router-link>
-    |
-    <router-link to="/statistics">统计</router-link>
-  </div>
 </template>
 
 <style lang="scss">
