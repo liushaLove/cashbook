4009fd3cffc4500937d741b8c5888d6f49aca59b
liushaLove <1270842602@qq.com>
2021-02-02 15:57:04 +0800

    添加NotFound页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -22,6 +23,10 @@ const routes: Array<RouteRecordRaw> = [
     path: '/statistics',
     name: 'Statistics',
     component: Statistics
+  },
+  {
+    path: '*',
+    component: NotFound
   }
 ];
 
