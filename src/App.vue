d8feac21bc8a6497b5603437f69417d8a9d92fe4
liushaLove <1270842602@qq.com>
2021-02-02 16:24:13 +0800

    重复三次布局提交

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@
 </template>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -10,7 +15,6 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -25,7 +25,7 @@ const routes: Array<RouteRecordRaw> = [
     component: Statistics
   },
   {
-    path: '*',
+    path: '/NotFound',
     component: NotFound
   }
 ];
