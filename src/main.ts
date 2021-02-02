2af43c5242d3928ce67124e360480b44db14fe9b
liushaLove <1270842602@qq.com>
2021-02-02 21:49:40 +0800

    创建Icon.vue

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,10 @@ import router from './router'
 import store from './store'
 import Nav from '@/components/Nav.vue'
 import Layout from '@/components/Layout.vue';
+import Icon from '@/components/Icon.vue';
 
-createApp(App).component('Nav',Nav).component('Layout',Layout).use(store).use(router).mount('#app');
+createApp(App).component('Nav',Nav).
+component('Layout',Layout).
+component('Icon',Icon).
+use(store).use(router).
+mount('#app');
