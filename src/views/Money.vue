66cd639f79569c6d7e52fef46bb13b19d7aebe90
liushaLove <1270842602@qq.com>
2021-03-06 23:34:12 +0800

    将 create 封装到 recordListModel

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -41,9 +41,7 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
   saveRecord() {
-    const record2:RecordItem = recordListModel.clone(this.record);
-    record2.createAt = new Date();
-    this.recordList.push(record2);
+    recordListModel.create(this.record);
   }
   @Watch('recordList')
   onRecordListChange() {
