3529a2d5963f7ef4d95a6459600ea6e790afd63f
liushaLove <1270842602@qq.com>
2021-03-06 21:23:32 +0800

    添加标签可跳转至编辑页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,28 +1,27 @@
 <template>
   <Layout>
-    <ol class="tags">
-        <li v-for="tag in tags" :key="tag.id">
-          <span>{{tag.name}}</span>
-          <Icon name="right"/>
-        </li>
-    </ol>
+    <div class="tags">
+        <router-link class="tag"
+                       v-for="tag in tags" :key="tag.id"
+                       :to="`/labels/edit/${tag.id}`">
+            <span>{{tag.name}}</span>
+            <Icon name="right"/>
+        </router-link>
+    </div>
     <div class="createTag-wrapper">
       <button class="createTag" @click="createTag">新建标签</button>
     </div>
   </Layout>
 </template>
-
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import tagListModel from '@/models/tagListModel';
-
 tagListModel.fetch();
 @Component
-export default class Labels extends Vue{
+export default class Labels extends Vue {
   tags = tagListModel.data;
-
-  createTag(){
+  createTag() {
     const name = window.prompt('请输出标签名');
     if (name) {
       const message = tagListModel.create(name);
@@ -35,23 +34,25 @@ export default class Labels extends Vue{
   }
 }
 </script>
-
 <style lang="scss" scoped>
 .tags {
   background: white;
   font-size: 16px;
   padding-left: 16px;
   > li {
-    min-height: 44px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    border-bottom: 1px solid #e6e6e6;
-    svg {
-      width: 18px;
-      height: 18px;
-      color: #666;
-      margin-right: 16px;
+    > .tag {
+      min-height: 44px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      border-bottom: 1px solid #e6e6e6;
+
+      svg {
+        width: 18px;
+        height: 18px;
+        color: #666;
+        margin-right: 16px;
+      }
     }
   }
 }
