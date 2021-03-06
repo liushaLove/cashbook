85610432bc394dcb9891c92fa51a6003d893cce2
liushaLove <1270842602@qq.com>
2021-03-06 20:53:19 +0800

    展示和创建标签

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,15 +15,17 @@ import Types from '@/components/money/Types.vue';
 import Notes from '@/components/money/Notes.vue';
 import Tags from '@/components/money/Tags.vue';
 import {Component,Watch} from 'vue-property-decorator';
-import model from '@/model';
+import recordListModel from '@/models/recordListModel';
+import tagListModel from '@/models/tagListModel';
 
-const recordList = model.fetch();
+const recordList = recordListModel.fetch();
+const tagList = tagListModel.fetch();
 
 @Component({
   components:{Tags,Types,NumberPad,Notes}
 })
 export default class Money extends Vue {
-  tags = ['衣', '食', '住', '行', '彩票'];
+  tags = tagList;
   recordList:RecordItem[] = recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
@@ -35,13 +37,13 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
   saveRecord() {
-    const record2:RecordItem = model.clone(this.record);
+    const record2:RecordItem = recordListModel.clone(this.record);
     record2.createAt = new Date();
     this.recordList.push(record2);
   }
   @Watch('recordList')
   onRecordListChange() {
-    model.save(this.recordList);
+    recordListModel.save(this.recordList);
   }
 }
 </script>
