fd385f99f49f65d64f828bc7dbabd00f9dae1c89
liushaLove <1270842602@qq.com>
2021-03-06 23:28:43 +0800

    重新封装 recordListModel

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -47,7 +47,7 @@ export default class Money extends Vue {
   }
   @Watch('recordList')
   onRecordListChange() {
-    recordListModel.save(this.recordList);
+    recordListModel.save();
   }
 }
 </script>
