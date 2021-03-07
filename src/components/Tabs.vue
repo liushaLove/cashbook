7a54e7093410bc4a792fbccc019855232f745b72
liushaLove <1270842602@qq.com>
2021-03-07 10:05:51 +0800

    添加 height 示例

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -20,6 +20,8 @@ export default class Tabs extends Vue {
   @Prop({required: true, type: Array}) dataSource!: DataSourceItem[];
   @Prop(String) readonly value!: string;
   @Prop(String) classPrefix?: string;
+  @Prop({type: String, default: '64px'})
+  height!: string;
 
   liClass(item: DataSourceItem) {
     return {
